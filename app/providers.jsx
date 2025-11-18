a3347aeb62b7be0e31780b6315a3591b67911dd4
Apple <apple@Apples-MacBook-Pro.local>
2025-11-18 15:19:55 +0530

    Done 22

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from '@vercel/analytics/next';
 
 /**
  * Client component to wrap the entire application in the NextAuth SessionProvider.
  * This makes the session context available to all client components using useSession().
  */
 export function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>
+    {children}
+    <Analytics />
+    </SessionProvider>;
 }
